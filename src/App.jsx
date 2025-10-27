c099182269bfdd0c9b6ac65bd96e1febc79e03e2
Kowshiga16 <svkowshiga@gmail.com>
2025-10-27 16:24:34 +0530

    Fix import path for Main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Main from './components/main'
+import Main from './components/Main.jsx';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
      <Main></Main>
     </>
   )
